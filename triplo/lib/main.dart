6eb27c5763f966222e42ccc3dc4c271516d39318
Giulio Angaroni <giulio.angaroni@mail.polimi.it>
2025-10-29 17:05:10 +0100

    Login and Registration page

--- triplo/lib/main.dart
+++ triplo/lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:triplo/pages/login_page/LoginPage.dart';
 import 'pages/home-page.dart';
 import 'pages/splash-screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:triplo/pages/registration_page/registration_page.dart';
+import 'firebase_options.dart';
+import '/pages/login_page/LoginPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,7 +29,17 @@ class MyApp extends StatelessWidget {
       ),
       //home: const MyHomePage(),
       //home: const SplashScreen(),
-      home: LoginPage(),
+      //home: RegistrationPage(),
+
+
+      initialRoute: '/login',
+
+      routes: {
+        '/login' : (context) => LoginPage(),
+        '/registration' : (context) => RegistrationPage(),
+
+      },
+
     );
   }
 }
